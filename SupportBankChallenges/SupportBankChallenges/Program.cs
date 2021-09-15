5dac91a6ccdd7fc64b52fc8834361748269df061
Rhys Stedman <R.Stedman@ucas.ac.uk>
2021-09-15 15:48:01 +0100

    Improvements to the work in itself, trying to understand how everything works and getting it to a semi working state

--- SupportBankChallenges/SupportBankChallenges/Program.cs
+++ SupportBankChallenges/SupportBankChallenges/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SupportBankChallenges
 {
@@ -10,7 +11,35 @@ namespace SupportBankChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string path = @"C:\Repos\SuppBankChal\Transactions2014.txt";
+            var listAll = Console.ReadLine();
+            var listAccount = Console.ReadLine();
+
+            Dictionary<string, int> listingPeopleMoney = new Dictionary<string, int>();
+            
+            //Spreadsheet Format
+            // Date, From, To, Narrative, Amount
+            //Potentially use an array so - 0 1 2 3 4
+
+            if (listAll == "List All")
+            {
+                Console.WriteLine("testworks");
+            }
+
+            string[] lines = File.ReadAllLines(path);
+            foreach(string line in lines)
+            {
+                string[] columns = line.Split(',');
+                foreach(string coloumn in columns)
+                {
+                    Console.WriteLine();
+                    Console.ReadLine();
+                }
+                Console.WriteLine();
+                
+            }
+
+
         }
     }
 }
---
+++ SupportBankChallenges/SupportBankChallenges/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportBankChallenges
+{
+    class Transaction
+    {
+        public decimal amount;
+        //Add more properties
+        string to;
+        string from;
+        string date;
+    }
+}
